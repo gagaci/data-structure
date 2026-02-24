8e5ba795d64b9a9104841a97e761fab76177ac66
khabibulloh <bayramovxabibulloh@gmail.com>
2026-02-24 22:37:07 +0500

    implementing stack with array and linked list

---
+++ stack_array.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+class myStack {
+int *arr; // array to store elements
+int capacity; // maximum size of stack
+int top; // index of top element
+public:
+myStack(int cap) {
+capacity = cap;
+arr = new int[capacity];
+top = -1;
+}
+
+void push(int x){
+if(top == capacity - 1){
+    cout << "Stack Overflow\n";
+    return;
+ }
+
+ arr[++top] = x;
+}
+
+int pop(){
+    if (top == -1){
+      cout << "Stack Underflow\n";  
+    }
+    return arr[top--];
+}
+
+int peek(){
+    if (top == -1){
+      cout << "Stack Underflow\n";  
+    }
+    return arr[top];
+}
+
+bool isEmpty(){
+    return top == -1;
+}
+
+};
+
+
+int main(){
+    myStack s(5);
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    cout << "Peek: " << s.peek() << endl;   // 30
+
+    cout << "Pop: " << s.pop() << endl;     // 30
+    cout << "Pop: " << s.pop() << endl;     // 20
+
+    cout << "Peek: " << s.peek() << endl;   // 10
+
+    cout << "Is Empty: " << s.isEmpty() << endl; // 0
+
+    s.pop();
+    cout << "Is Empty: " << s.isEmpty() << endl; // 1
+
+    s.pop(); // Stack Underflow
+
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    s.push(6); // Stack Overflow
+
+    return 0;
+}
---
+++ stack_linked_list.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+
+class Node {
+public:
+int data;
+Node* next;
+Node(int x) {
+data = x;
+next = NULL;
+}
+};
+
+class myStack {
+// pointer to top node
+Node* top;
+public:
+myStack() {
+// initially stack is empty
+top = NULL;
+}
+
+void push(int x){
+    Node* temp = new Node(x);
+    temp->next = top;
+    top = temp;
+}
+
+int pop() {
+if(top == NULL){
+    cout << "Stack Underflow" << endl;
+    return -1;
+}
+Node* temp = top;
+top = temp->next;
+int x = temp->data;
+delete temp;
+return x;
+}
+
+int peek(){
+    if(top == NULL){
+    cout << "Stack Underflow" << endl;
+    return -1;
+    }
+    return top->data;
+}
+
+int isEmpty(){
+    return top == NULL;
+}
+
+};
+
+
+int main(){
+    myStack s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    cout << "Peek: " << s.peek() << endl;   // 30
+
+    cout << "Pop: " << s.pop() << endl;     // 30
+    cout << "Pop: " << s.pop() << endl;     // 20
+
+    cout << "Peek: " << s.peek() << endl;   // 10
+
+    cout << "Is Empty: " << s.isEmpty() << endl; // 0
+
+    s.pop();
+    cout << "Is Empty: " << s.isEmpty() << endl; // 1
+
+    s.pop(); // Stack Underflow
+
+    return 0;
+}
