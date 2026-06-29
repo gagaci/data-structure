131813c5d5626b6c7b21e351f8ccc47f72290d52
khabibulloh <bayramovxabibulloh@gmail.com>
2026-06-29 13:22:15 +0500

    new: has cycle done with visualization

---
+++ java/has-cycle-linked-list/HasCycleLinkedList.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HasCycleLinkedList {
+
+  public boolean hasCycle(ListNode head) {
+    Set<ListNode> set = new HashSet<>();
+    ListNode temp = head;
+
+    while (temp != null) {
+
+      if (!set.add(temp)) {
+        return true;
+      }
+      temp = temp.next;
+    }
+    return false;
+  }
+}
